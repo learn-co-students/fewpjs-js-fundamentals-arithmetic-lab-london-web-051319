f7471ea46ef53e00f0609be01403018fa2f9d38b
James <hayward.jp@gmail.com>
2019-06-30 12:12:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+
+const multiply = num1*num2;
+
+let random = Math.random();
+Math.floor(random);
+
+const mod = (num1 + 1)/8;
+console.log(mod);
+
+const max = Math.max(5, 2, 7, 10, 3, 15, 20);
