008b8cb573f1e6d061ac0b594662070bf19b58fd
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-07-01 10:41:22 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+// import { returnStatement } from "../../../../../../../../Library/Caches/typescript/3.5/node_modules/@types/babel-types";
 
-// Write your code here
+// // Write your code here
+
+// function multiply() {
+//     let num1 = 2;
+//     let num2 = 31;
+//     return num1 * num2
+// };
+
+// function random() {
+//     return Math.floor(Math.random() * 100) + 1
+// };
+
+// function mod() {
+//     let num3 = 16;
+//     let num4 = 6;
+//     return num3 % num4
+// };
+
+// function max() {
+//     return Math.max( 5, 10, 15, 20 )
+// };
+
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let mod = 20 % 16;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let max = Math.max( 5, 10, 15, 20 );
